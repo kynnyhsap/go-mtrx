95400e1b85828a982b3d404099a4d19633434dd3
Andrew Pashinnik <tobirawork@gmail.com>
2018-09-04 19:25:42 +0300

    feat(matrix): add getRow, getColumn and getDimention methods add Dimention type rename and refactor count rows/columns methods

--- matrix.go
+++ matrix.go
@@ -1,7 +1,42 @@
-package main
+package matrix
 
 // Matrix -
 type Matrix [][]int
+
+// Dimention - the dimention of matrix
+type Dimention struct {
+	rows    int
+	columns int
+}
+
+func (m Matrix) getDimention() Dimention {
+	return Dimention{
+		rows:    m.getRowsCount(),
+		columns: m.getColumnsCount(),
+	}
+}
+
+func (m Matrix) getRowsCount() int {
+	if !m.isValid() {
+		return 0
+	}
+
+	return len(m)
+}
+
+func (m Matrix) getColumnsCount() int {
+	if !m.isValid() {
+		return 0
+	}
+
+	if len(m) > 0 {
+		firstRow := m[0]
+
+		return len(firstRow)
+	}
+
+	return 0
+}
 
 func (m Matrix) isValid() bool {
 	if len(m) == 0 {
@@ -23,29 +58,7 @@ func (m Matrix) isSquareMatrix() bool {
 		return false
 	}
 
-	return len(m) == len(m[0])
-}
-
-func (m Matrix) countRows() int {
-	if !m.isValid() {
-		return 0
-	}
-
-	return len(m)
-}
-
-func (m Matrix) countColumns() int {
-	if !m.isValid() {
-		return 0
-	}
-
-	if len(m) > 0 {
-		firstRow := m[0]
-
-		return len(firstRow)
-	}
-
-	return 0
+	return m.getRowsCount() == m.getColumnsCount()
 }
 
 func (m Matrix) isNullMatrix() bool {
@@ -64,6 +77,64 @@ func (m Matrix) isNullMatrix() bool {
 	return true
 }
 
-func main() {
+func (m Matrix) isDiagonalMatrix() bool {
+	if !m.isSquareMatrix() {
+		return false
+	}
 
+	if m.getRowsCount() < 2 {
+		return false
+	}
+
+	for diagonalIndex := 0; diagonalIndex < m.getRowsCount(); diagonalIndex++ {
+		row := m[diagonalIndex]
+
+		for rowIndex := 0; rowIndex < len(row); rowIndex++ {
+			isZero := row[rowIndex] == 0
+
+			if rowIndex == diagonalIndex && isZero {
+				return false
+			}
+
+			if rowIndex != diagonalIndex && !isZero {
+				return false
+			}
+
+		}
+	}
+
+	return true
+}
+
+func (m Matrix) getRow(n int) []int {
+	if !m.isValid() {
+		return []int{}
+	}
+
+	if m.getRowsCount() < n || n < 1 {
+		return []int{}
+	}
+
+	return m[n-1]
+}
+
+func (m Matrix) getColumn(n int) []int {
+	if !m.isValid() {
+		return []int{}
+	}
+
+	if n < 1 || m.getColumnsCount() < n {
+		return []int{}
+	}
+
+	println(n)
+
+	var column []int
+	for i := 0; i < m.getRowsCount(); i++ {
+		row := m[i]
+
+		column = append(column, row[n-1])
+	}
+
+	return column
 }
