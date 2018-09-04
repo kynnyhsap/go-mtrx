9daa109bfef54671b4b0e757908883c332b47d15
Andrew Pashinnik <tobirawork@gmail.com>
2018-09-04 17:31:16 +0300

    feat: add countColumns, countRows and isNullMatrix

--- matrix.go
+++ matrix.go
@@ -18,12 +18,50 @@ func (m Matrix) isValid() bool {
 	return true
 }
 
-func (m Matrix) isSquare() bool {
+func (m Matrix) isSquareMatrix() bool {
 	if !m.isValid() {
 		return false
 	}
 
 	return len(m) == len(m[0])
+}
+
+func (m Matrix) countRows() int {
+	if !m.isValid() {
+		return 0
+	}
+
+	return len(m)
+}
+
+func (m Matrix) countColumns() int {
+	if !m.isValid() {
+		return 0
+	}
+
+	if len(m) > 0 {
+		firstRow := m[0]
+
+		return len(firstRow)
+	}
+
+	return 0
+}
+
+func (m Matrix) isNullMatrix() bool {
+	if !m.isValid() {
+		return false
+	}
+
+	for _, row := range m {
+		for _, number := range row {
+			if number != 0 {
+				return false
+			}
+		}
+	}
+
+	return true
 }
 
 func main() {
